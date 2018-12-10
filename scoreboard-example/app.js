40f5b667ce959a31ad83ff48bb890edf9ec6d00d
Linda Lai <lindalai.615@gmail.com>
2018-12-11 02:52:18 +1100

    Add update and delete functions to main App components

--- scoreboard-example/app.js
+++ scoreboard-example/app.js
@@ -11,6 +11,7 @@ const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
+                <button className="remove-player" onClick={ () => props.removePlayer(props.id)}>x</button>
                 { props.name }
             </span>
 
@@ -19,21 +20,22 @@ const Player = (props) => {
     );
 }
 
+// Create a stateful component by first defining an initial state inside the Counter class
+// Refactored score into a state object, instead of getting this from props
 class Counter extends React.Component {
-    // Moved score into a state object, instead of getting this from props
     state = {
         score: 0
     };
 
     // Whenever the click event handler is triggered, the score will be increment/decremented
-    // State is never modified directly - only by using setState() method to indicate state has changed
-    // .setState function also takes a callback for async code
+    // State is never modified directly - the only way React allows is using its inbuilt setState() method to indicate state has changed
+    // State updates may be asynchronous, so it may not always lead to the component re-rendering with new data, and could cause state inconsistency.
+    // setState() accepts a callback function that produces state based on the previous state in a more reliable way
     incrementScore = () => {
         this.setState( prevState => ({
             score: prevState.score + 1
         }));
     }
-    
     decrementScore = () => {
         this.setState( prevState => ({
           score: prevState.score - 1
@@ -41,6 +43,8 @@ class Counter extends React.Component {
     }
 
     // If either props or state changes, React executes the render() method to update what gets displayed to user
+    // Create an event handler that updates state, using React's inbuilt setState() method
+    // Give the button an onclick event that calls the event handler when clicked
     render() {
         return (
             <div className="counter">
@@ -52,8 +56,8 @@ class Counter extends React.Component {
     }
 }
 
+// Initialize a players state in the App component, then create and wire up an event handler that removes a player onClick
 class App extends React.Component {
-
     state = {
         players: [
             {
@@ -74,7 +78,14 @@ class App extends React.Component {
             }
         ]
     };
-
+    // Use the filter() array iteration method to remove an object from the players array in state
+    handleRemovePlayer = (id) => {
+        this.setState( prevState => {
+            return {
+                players: prevState.players.filter(p => p.id !== id)
+            };
+        });
+    }
     render() {
         return (
             <div className="scoreboard">
@@ -86,7 +97,9 @@ class App extends React.Component {
                 { this.state.players.map( player =>
                     <Player
                         name={ player.name }
+                        id={ player.id }
                         key={ player.id.toString() }
+                        removePlayer={this.handleRemovePlayer}
                     />
                 )}
             </div>
